637e2751c140120cd16ecb78f464de6c7afcf538
Graham Christensen <graham@grahamc.com>
2019-02-05 09:27:00 -0500

    Update report, add explainer

--- report.sh
+++ report.sh
@@ -6,6 +6,8 @@
     rev=$(git rev-parse HEAD)
     cd ..
 
+    cp ./reproducibility-log "./public/reproducibility-log-$rev"
+
     reproducible=$(cat ./reproducibility-log | grep '^reproducible' | wc -l)
     total=$(cat ./reproducibility-log | wc -l)
     percent=$(printf "%.3f" "$(echo "($reproducible / $total) * 100" | bc -l)")
@@ -13,7 +15,7 @@
     cat <<EOF
 <html>
 <head>
-<title>NixOS's minimal ISO is $percent% reproducible!</title>
+<title>Is NixOS Reproducible?</title>
 <meta name="description" content="nixos-unstable's iso_minimal.x86_64-linux build is $percent% reproducible!" />
 
 <!-- Twitter Card data -->
@@ -41,6 +43,7 @@ nix-build ./nixos/release-combined.nix -A nixos.iso_minimal.x86_64-linux
 <h1 style="color: green">$reproducible / $total ($percent%) are reproducible!</h1>
 <hr>
 <h3>unreproduced paths</h3>
+<ul>
 EOF
 
     for drv in $(cat ./reproducibility-log | awk '$1 == "unreproducible" { print $2; }'); do
@@ -50,7 +53,35 @@ EOF
     done
 
     cat <<EOF
+</ul>
+<p><a href="./reproducibility-log-$rev">full list of build results</a></p>
 <hr />
+<h3 id="test-circumstance">How are these tested?</h3>
+<p>Each build is run twice, at different times, on different hardware
+running different kernels.</p>
+
+<h3 id="result-confidence">How confident can we be in the results?</h3>
+
+<p>Fairly. We don't currently inject randomness at the filesystem
+layer, but many of the reproducibility issues are being exercised
+already. It isn't possible to <em>guarantee</em> a package is
+reproducible, just like it isn't possible to prove software is
+bug-free. It is possible there is nondeterminism in a package source,
+waiting for the some specific circumstance.</p>
+
+<p>This is why we run these tests: to track how we are doing over
+time, to submit bug fixes for nondeterminism when we find them.</p>
+
+<h3 id="next-steps">How can we do better?</h3>
+
+<p>There are further steps we could take. For example, the next likely
+step is using
+<a href="https://salsa.debian.org/reproducible-builds/disorderfs">disorderfs</a>
+which injects additional nondeterminism by reordering directory entries.
+</p>
+
+<hr />
+
 <small>Generated at $(TZ=UTC date) from
 <a href="https://github.com/grahamc/r13y.com">https://github.com/grahamc/r13y.com</a></small>
 <center><img style="max-width: 100px" src="https://nixos.org/logo/nixos-logo-only-hires.png" /></center>
