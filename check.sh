88af699f2ffcffe6a61971e757c888dcca02e2a0
Graham Christensen <graham@grahamc.com>
2019-07-20 10:47:57 -0400

    Only run r13y.com if it isn't really hot

--- check.sh
+++ check.sh
@@ -6,11 +6,24 @@ set -eux
 #export LANG=en_US.UTF-8
 #export LOCALE_ARCHIVE=/run/current-system/sw/lib/locale/locale-archive
 
+function fetch_temps() (
+    curl 'https://api.weather.gov/gridpoints/ALY/76,52/forecast/hourly?units=si' \
+        | jq -r '.properties.periods | limit(24;.[]) | .temperature | select(. > 26)'
+)
+function check_temp() {
+    if [ $(fetch_temps | wc -l) -gt 5 ]; then
+        echo "too hot";
+        exit 1
+    fi
+}
+
 function nixpkgs_rev() (
     curl https://channels.nix.gsc.io/nixos-unstable-small/latest | cut -d' ' -f1
 )
 
 function main() {
+    check_temp
+
     export REV=$(nixpkgs_rev)
     export HASH=$(nix-prefetch-url --unpack "https://github.com/NixOS/nixpkgs/archive/${REV}.tar.gz")
     export RUST_BACKTRACE=1
