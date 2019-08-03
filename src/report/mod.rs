2f4723ee34fc42a176653303a2061d95dce1a26f
tilpner <till@hoeppner.ws>
2019-08-03 18:05:17 +0200

    Pull report operation into library

--- src/report/mod.rs
+++ src/report/mod.rs
@@ -1,121 +1,36 @@
-extern crate r13y;
-extern crate serde;
-#[macro_use]
-extern crate log;
-extern crate chrono;
-extern crate digest;
-extern crate env_logger;
-extern crate rand;
-extern crate serde_json;
-extern crate sha2;
-extern crate tempdir;
 use chrono::Utc;
-use r13y::contentaddressedstorage::ContentAddressedStorage;
-use r13y::derivation::Derivation;
-use r13y::diffoscope::Diffoscope;
-use r13y::messages::{Attr, BuildRequest, BuildRequestV1, BuildResponseV1, BuildStatus, Subset};
-use std::collections::HashSet;
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::{BufRead, Write};
-use std::path::{Path, PathBuf};
-use std::process::Command;
 
-fn main() {
-    env_logger::init();
+use crate::{
+    cas::ContentAddressedStorage,
+    derivation::Derivation,
+    diffoscope::Diffoscope,
+    eval::eval,
+    messages::{BuildRequest, BuildResponseV1, BuildStatus},
+};
 
-    let instruction = BuildRequest::V1(BuildRequestV1 {
-        nixpkgs_revision: env::args().nth(1).unwrap(),
-        nixpkgs_sha256sum: env::args().nth(2).unwrap(),
-        result_url: "bogus".into(),
-        subsets: vec![(
-            Subset::NixOSReleaseCombined,
-            Some(vec![vec![
-                "nixos".into(),
-                "iso_minimal".into(),
-                "x86_64-linux".into(),
-            ]]),
-        )]
-        .into_iter()
-        .collect(),
-    });
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::{Path, PathBuf},
+};
 
+pub fn report(instruction: BuildRequest) {
     let job = match instruction {
         BuildRequest::V1(ref req) => req.clone(),
     };
+
     let tmpdir = PathBuf::from("./tmp/");
     let results: Vec<BuildResponseV1> = serde_json::from_reader(
         File::open(format!("reproducibility-log-{}.json", job.nixpkgs_revision)).unwrap(),
     )
     .unwrap();
 
-    let mut to_build: HashSet<String> = HashSet::new();
-
-    for (subset, attrs) in job.subsets.iter() {
-        let drv = {
-            let mut drv = tmpdir.clone();
-            drv.push("result.drv");
-            drv
-        };
-        let path: PathBuf = Into::<&'static Path>::into(subset).to_path_buf();
-        let attrs: Vec<Attr> = attrs.clone().unwrap_or(vec![]);
-
-        info!("Evaluating {:?} {:#?}", &subset, &attrs);
-        let eval = Command::new("nix-instantiate")
-            // .arg("--pure-eval") // See evaluate.nix for why this isn't passed yet
-            .arg("-E")
-            .arg(include_str!("./evaluate.nix"))
-            .arg("--add-root")
-            .arg(&drv)
-            .arg("--indirect")
-            .args(&[
-                "--argstr",
-                "revision",
-                &job.nixpkgs_revision,
-                "--argstr",
-                "sha256",
-                &job.nixpkgs_sha256sum,
-                "--argstr",
-                "subfile",
-                &format!("{}", path.display()),
-                "--argstr",
-                "attrsJSON",
-                &serde_json::to_string(&attrs).unwrap(),
-            ])
-            .output()
-            .expect("failed to execute process");
-
-        for line in eval.stderr.lines() {
-            info!("stderr: {:?}", line)
-        }
-        for line in eval.stdout.lines() {
-            debug!("stdout: {:?}", line)
-        }
-
-        let query_requisites = Command::new("nix-store")
-            .arg("--query")
-            .arg("--requisites")
-            .arg(&drv)
-            .output()
-            .expect("failed to execute process");
-        for line in query_requisites.stderr.lines() {
-            info!("stderr: {:?}", line);
-        }
-        for line in query_requisites.stdout.lines() {
-            debug!("stdout: {:?}", &line);
-            if let Ok(line) = line {
-                if line.ends_with(".drv") {
-                    to_build.insert(line.into());
-                }
-            }
-        }
-    }
+    let to_build = eval(instruction.clone());
 
     let report_dir = PathBuf::from("./report/");
-    std::fs::create_dir_all(&report_dir).unwrap();
+    fs::create_dir_all(&report_dir).unwrap();
     let diff_dir = PathBuf::from("./report/diff");
-    std::fs::create_dir_all(&diff_dir).unwrap();
+    fs::create_dir_all(&diff_dir).unwrap();
     let mut html = File::create(report_dir.join("index.html")).unwrap();
 
     let read_cas = ContentAddressedStorage::new(tmpdir.clone());
@@ -126,10 +41,11 @@ fn main() {
     let mut unreproducible_list: Vec<String> = vec![];
     let mut unchecked = 0;
     let mut first_failed: Vec<String> = vec![];
+
     for response in results.into_iter().filter(|response| {
         (match response.request {
             BuildRequest::V1(ref req) => req.nixpkgs_revision == job.nixpkgs_revision,
-        }) && to_build.contains(&response.drv)
+        }) && to_build.contains(&PathBuf::from(&response.drv))
     }) {
         total += 1;
         match response.status {
@@ -180,7 +96,7 @@ fn main() {
                         // <li><a href="./diff/59nzffg69nprgg2zp8b36rqwha8vxzjk-perl-5.28.1.drv.html">(diffoscope)</a> <a href="./nix/store/59nzffg69nprgg2zp8b36rqwha8vxzjk-perl-5.28.1.drv">(drv)</a> <code>/nix/store/59nzffg69nprgg2zp8b36rqwha8vxzjk-perl-5.28.1.drv</code></li>
                     }
                 }
-                unreproducible_list.push(format!("</ul></li>"));
+                unreproducible_list.push("</ul></li>".to_string());
 
                 println!("{:#?}", hashes);
             }
