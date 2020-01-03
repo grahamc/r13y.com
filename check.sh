5dd9945849695f63e9435e7d2c499e2dbe593ecc
Graham Christensen <graham@grahamc.com>
2020-01-03 17:28:21 -0500

    Update to 19.09, delete the check_temp stuff, open up to the standard scope

--- check.sh
+++ check.sh
@@ -1,36 +1,23 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash ./default.nix -I nixpkgs=channel:nixos-19.03
+#!nix-shell -i bash ./default.nix -I nixpkgs=channel:nixos-19.09
 
 set -eux
 
 #export LANG=en_US.UTF-8
 #export LOCALE_ARCHIVE=/run/current-system/sw/lib/locale/locale-archive
 
-function fetch_temps() (
-    curl 'https://api.weather.gov/gridpoints/ALY/76,52/forecast/hourly?units=si' \
-        | jq -r '.properties.periods | limit(24;.[]) | .temperature | select(. > 26)'
-)
-function check_temp() {
-    if [ $(fetch_temps | wc -l) -gt 5 ]; then
-        echo "too hot";
-        exit 1
-    fi
-}
-
 function nixpkgs_rev() (
     curl https://channels.nix.gsc.io/nixos-unstable-small/latest | cut -d' ' -f1
 )
 
 function main() {
-    check_temp
-
     export REV=$(nixpkgs_rev)
     export HASH=$(nix-prefetch-url --unpack "https://github.com/NixOS/nixpkgs/archive/${REV}.tar.gz")
     export SUBSET=nixos:nixos.iso_minimal.x86_64-linux
     export RUST_BACKTRACE=1
     (unset RUST_LOG; cargo build)
 
-    SUBSET="nixpkgs:stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.binutils"
+    # SUBSET="nixpkgs:stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.stdenv.__bootPackages.binutils"
     cargo run -- \
           --subset "$SUBSET" \
           --rev "$REV" \
